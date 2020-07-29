eabc7f1c218c988bd918b97e1e824d3d01b7068e
lleo19 <ll200811@nemere.net>
2020-07-29 14:49:28 -0400

    Update media_player.py

--- media_player.py
+++ media_player.py
@@ -1,1 +1,28 @@
+import logging
+import voluptuous as vol
 
+from homeassistant.components.media_player import MediaPlayerEntity
+from homeassistant.components.media_player.const import (
+    ATTR_MEDIA_ENQUEUE,
+    MEDIA_TYPE_MUSIC,
+    MEDIA_TYPE_PLAYLIST,
+    SUPPORT_CLEAR_PLAYLIST,
+    SUPPORT_NEXT_TRACK,
+    SUPPORT_PAUSE,
+    SUPPORT_PLAY,
+    SUPPORT_PLAY_MEDIA,
+    SUPPORT_PREVIOUS_TRACK,
+    SUPPORT_SEEK,
+    SUPPORT_SELECT_SOURCE,
+    SUPPORT_SHUFFLE_SET,
+    SUPPORT_STOP,
+    SUPPORT_VOLUME_MUTE,
+    SUPPORT_VOLUME_SET,
+)
+from homeassistant.const import (
+    ATTR_TIME,
+    EVENT_HOMEASSISTANT_STOP,
+    STATE_IDLE,
+    STATE_PAUSED,
+    STATE_PLAYING,
+)
