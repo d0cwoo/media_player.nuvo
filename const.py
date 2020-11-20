15b74cb49f7994b253122481011947ad941a3e3f
d0cwoo <74459074+d0cwoo@users.noreply.github.com>
2020-11-20 13:32:56 -0600

    Add files via upload

---
+++ const.py
@@ -0,0 +1,24 @@
+"""Constants for the Nuvo 6-Zone Amplifier Media Player component."""
+
+DOMAIN = "nuvo"
+CONF_MODEL = "essentia"
+
+CONF_SOURCES = "sources"
+
+CONF_SOURCE_1 = "source_1"
+CONF_SOURCE_2 = "source_2"
+CONF_SOURCE_3 = "source_3"
+CONF_SOURCE_4 = "source_4"
+CONF_SOURCE_5 = "source_5"
+CONF_SOURCE_6 = "source_6"
+
+CONF_ZONES = "zones"
+
+CONF_NOT_FIRST_RUN = "not_first_run"
+
+SERVICE_SNAPSHOT = "snapshot"
+SERVICE_RESTORE = "restore"
+
+FIRST_RUN = "first_run"
+NUVO_OBJECT = "nuvo_object"
+UNDO_UPDATE_LISTENER = "update_update_listener"
